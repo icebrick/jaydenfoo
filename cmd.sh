90d4619f4551f9d19a0f4896fd83c6e5574baf52
jayden <icebrick@163.com>
2018-05-31 18:52:41 +0800

    *修改docker部署配置

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-STATIC_DIR=./static_collect
-
-if [ -d "$STATIC_DIR" ]; then
-    rm -r "$STATIC_DIR"
-fi
-
-python3 manage.py collectstatic
+#STATIC_DIR=./static_collect
+#
+#if [ -d "$STATIC_DIR" ]; then
+#    rm -r "$STATIC_DIR"
+#fi
+#
+#python3 manage.py collectstatic
 cd deploy
 docker-compose up --build
